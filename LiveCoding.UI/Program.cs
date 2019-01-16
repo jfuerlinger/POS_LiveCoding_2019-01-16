d10b772f70d272dc0fe7c0dc057134902ff47b84
Josef Fürlinger <josef.fuerlinger@gmail.com>
2019-01-16 14:41:41 +0100

    Templated enhanced.

--- LiveCoding.UI/Program.cs
+++ LiveCoding.UI/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LiveCoding.UI
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = Enumerable.Empty<string>();
+            PrintResult("Ergebnis", result);
+
+            Console.WriteLine("Bitte eine beliebige Taste zum Beenden drücken.");
+        }
+
+        private static void PrintResult<T>(string caption, IEnumerable<T> enumeration)
+        {
+            ConsoleColor originColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine();
+            Console.WriteLine($"===================== {caption} ======================");
+            foreach (var item in enumeration)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"======================= ENDE ========================");
+            Console.WriteLine();
+
+            Console.ForegroundColor = originColor;
         }
     }
 }
